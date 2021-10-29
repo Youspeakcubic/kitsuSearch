cdef06b378ffa1d14d3d36c822cf0d3695efead3
Anthony <delantho327@gmail.com>
2021-10-29 17:03:51 -0500

    tried to new method to fetching queries WIP

--- assets/JS/animeQuery.js
+++ assets/JS/animeQuery.js
@@ -1,5 +1,6 @@
 let baseUrl = "https://kitsu.io/api/edge";
 let trendingUrl = "https://kitsu.io/api/edge/trending/anime"
+let query;
 // on load event listener
 window.onload = (event) => {
   console.log("pageStartUp")
@@ -7,11 +8,17 @@ window.onload = (event) => {
     .then(response => response.json())
     .then(data => console.log(data))
   console.log("Trending Anime loaded")
-};
-let querySearch = () => {
-  console.log("search starting");
-  let query = document.getElementById('searchTerm').value;
-  fetch(`${baseUrl}/anime?filter[text]=${query}`, {
+
+document.getElementById("searchButton").addEventListener("click", function() {
+  query = document.getElementById("searchTerm").value
+  console.log(query);
+  console.log(`${baseUrl}/anime?filter[text]=${query}`);
+  return fetchQuery();
+
+});
+let fetchQuery = () => {
+  console.log(`${query}`);
+  return fetch(`${baseUrl}/anime?filter[text]=${query}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/vnd.api+json',
@@ -20,4 +27,5 @@ let querySearch = () => {
     })
     .then(response => response.json())
     .then(data => console.log(data))
+};
 }
