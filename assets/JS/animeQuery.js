fdf5dd0a72738a737a82b9db4464809ac64acf92
Anthony <delantho327@gmail.com>
2021-11-10 07:28:04 -0600

    finished fetch functionality, trending animes and queries can now be pulled

--- assets/JS/animeQuery.js
+++ assets/JS/animeQuery.js
@@ -1,31 +1,28 @@
-let baseUrl = "https://kitsu.io/api/edge";
-let trendingUrl = "https://kitsu.io/api/edge/trending/anime"
-let query;
-// on load event listener
+let topUrl = "https://api.jikan.moe/v3/top/anime";
+//on load event listener
 window.onload = (event) => {
-  console.log("pageStartUp")
-  fetch(trendingUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
-  console.log("Trending Anime loaded")
+  fetch(topUrl)
+  .then (response => response.json())
+  .then (response => console.log(response))
 }
-document.getElementById("searchButton").addEventListener("click", function() {
-  query = document.getElementById("searchTerm");
-  console.log(query);
-  var url = new URL("https://kitsu.io/api/edge/anime?filter[text]="),{
-      parameters: {q: query},
-      method:'GET',
-      mode:'cors',
-      headers:{
-        Accept: 'application/vnd.api+json',
-        Content-Type : 'application/vnd.api+json'
-      },
+let fetchQuery = () => {
+  let query = document.getElementById('searchTerm').value;
+  if (!query) {
 
-    }
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  } else {
+    var baseUrl = new URL("https://api.jikan.moe/v3/search/anime?q="),
+      params = {
+        q: query
+      }
+    Object.keys(params).forEach(key => baseUrl.searchParams.append(key, params[key]))
     //we are appending the parameters to the url we defined to create a new url with our user inputted and static parameters.
-  fetch(url)
-    //once we fetch, we then take that response and parse it into json, we then use our callback for processing our data ( distribute() )
-    .then(response => response.json())
-    .then (data => console.log(data))
-  });
+    fetch(baseUrl)
+      //once we fetch, we then take that response and parse it into json, we then use our callback for processing our data ( distribute() )
+      .then(response => response.json())
+      .then(data => processQuery(data))
+  }
+}
+
+let processQuery = (results) => {
+  console.log(results);
+}
